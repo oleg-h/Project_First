f3b9bcdd3b7f72695f2be1e6a477707790647f31
TanyaS <logariett@gmail.com>
2013-06-23 17:34:47 +0300

    Reading from file (done)

---
+++ read_from_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys
+
+try:
+	infilename = sys.argv[1]
+except:
+	print "Usage: ", sys.argv[0], " infile outfile"
+ 	sys.exit(1)
+ 
+ifile = open(infilename, 'r')
+
+price = []
+supply = []
+demand = []
+
+for line in ifile :
+	ln = line.split(":")
+	price += [float(ln[0].strip())]
+	supply += [float(ln[1].strip())]
+	demand += [float(ln[2].strip())]
+ifile.close()
+
+return price, supply, demand
+#print supply
