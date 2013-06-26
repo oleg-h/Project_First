d99c7d639a15a99b869477e757937a5665e40ac8
TanyaS <logariett@gmail.com>
2013-06-26 22:28:38 +0300

    Gathering staff in main

--- read_from_file.py
+++ read_from_file.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 import sys
 
-try:
-	infilename = sys.argv[1]
-except:
-	print "Usage: ", sys.argv[0], " infile outfile"
- 	sys.exit(1)
- 
-ifile = open(infilename, 'r')
+def do(sa0, sa1):
+	try:
+		infilename = sa1
+	except:
+		print "Usage: ", sa0, " infile outfile"
+	 	sys.exit(1)
+	 
+	ifile = open(infilename, 'r')
 
-price = []
-supply = []
-demand = []
+	price = []
+	supply = []
+	demand = []
 
-for line in ifile :
-	ln = line.split(":")
-	price += [float(ln[0].strip())]
-	supply += [float(ln[1].strip())]
-	demand += [float(ln[2].strip())]
-ifile.close()
+	for line in ifile :
+		ln = line.split(":")
+		price += [float(ln[0].strip())]
+		supply += [float(ln[1].strip())]
+		demand += [float(ln[2].strip())]
+	ifile.close()
 
-#return price, supply, demand
-print price, supply, demand
+	return price, supply, demand
